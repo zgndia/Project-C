7582f4d835d8996feff2018dbf9ccf952f205e0d
Zgn <ozgunkerimgunsads@hotmail.com>
2024-12-31 22:10:51 +0300

    Release 1.3

--- setup.bat
+++ setup.bat
@@ -1,11 +1,6 @@
 @echo off
-:: Get the current directory of the .bat file
 set current_dir=%~dp0
-
 pip install -r requirements.txt
-
-:: Copy main.pyw to the shell:startup folder
 copy "%current_dir%main.pyw" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
-
 del requirements.txt
 del setup.bat
